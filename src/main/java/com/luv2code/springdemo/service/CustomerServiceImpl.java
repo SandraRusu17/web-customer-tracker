18b14259a449ea76e49932e1be6ed9e870fafecc
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-06-18 23:16:55 +0300

    Refactor: Add service layer

---
+++ src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo.service;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+    // need to inject customer dao, because service depends on dao ( dependency injection )
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    // now our service will delegate the beginning and the end of the transaction
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        return customerDAO.getCustomers();
+    }
+}
