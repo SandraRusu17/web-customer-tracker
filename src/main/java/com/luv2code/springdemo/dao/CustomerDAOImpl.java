d7f76f53e02c2c69013f36cf2c506e27081ba396
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-06-19 12:46:29 +0300

    Update Customer: Prepopulating the form

--- src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -1,15 +1,13 @@
 package com.luv2code.springdemo.dao;
 
-import java.util.List;
-
+import com.luv2code.springdemo.entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.luv2code.springdemo.entity.Customer;
+import java.util.List;
 
 @Repository
 public class CustomerDAOImpl implements CustomerDAO {
@@ -44,6 +42,18 @@ public class CustomerDAOImpl implements CustomerDAO {
         // save the customer
         currentSession.save(theCustomer);
 
+    }
+
+    @Override
+    public Customer getCustomer(int theId) {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // now retrieve/read from database using the primary key
+        Customer theCustomer = currentSession.get(Customer.class, theId);
+
+        return theCustomer;
     }
 
 }
